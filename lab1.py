ba49e811b049a097be4a4327243b69cbe8e7519b
vkhans07 <vkhans@berkeley.edu>
2025-12-15 14:50:02 -0800

    Implemented Basic Moving Average strategy

--- lab1.py
+++ lab1.py
@@ -11,7 +11,7 @@ from sklearn.metrics import mean_squared_error, r2_score
 
 
 # regime-filtered moving average model
-def get_data(ticker='SPY', start_date='2010-01-01', end_date='2024-01-01'):
+def get_data(ticker='SPY', start_date='2010-01-01', end_date='2025-01-01'):
     df = yf.download(ticker, start=start_date, end=end_date)
     vix = yf.download('^VIX', start=start_date, end=end_date)
     data = pd.DataFrame()
@@ -28,7 +28,28 @@ def apply_strategy(data, shorter_window=50, longer_window=200, vix_threshold=20)
     data['Daily Log Return'] = np.log(data['Price'] / data['Price'].shift(1))
     # i think this is fine? it just drops off the first monday when we don't have a return
     data.dropna(inplace=True)
+    data['Shorter MA'] = data['Price'].rolling(window=shorter_window).mean()
+    data['Longer MA'] = data['Price'].rolling(window=longer_window).mean()
+
+    # generate signals: we want to long if shorter ma > longer than ma and vix < threshold
+    # otherwise, we stay neutral
+    data['Signal'] = np.where((data['Shorter MA'] > data['Longer MA']) &
+                              (data['VIX'] < vix_threshold), 1, 0)
+    
+    return data
+
+def backtest(data):
+    # you're not clairvoyant; you calculate signals at close so
+    # you can only act on them the next day
+    # so gng you gotta shift everything back by 1
+    # assume your df is well-formatted
+    data['Strategy Return'] = data['Signal'].shift(1) * data['Daily Log Return']
+    data.dropna(inplace=True)
+    data['Long Strategy Return'] = data['Daily Log Return'].cumsum().apply(np.exp)
+    data['MA Strategy Return'] = data['Strategy Return'].cumsum().apply(np.exp)
+
 
 data = get_data()
 apply_strategy(data)
-print(data.head())
+backtest(data)
+print(data.tail())
